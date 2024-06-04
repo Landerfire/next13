79a421fd3489c0e5e63b085060b285d77ea48f85
Landerfire <alanderlex@gmail.com>
2024-06-05 01:21:24 +0500

    getPosts=>services

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,6 @@
+import { getAllPosts as getData } from '@/services/getPosts';
 import { Metadata } from 'next';
 import Link from 'next/link';
-
-async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    next: { revalidate: 60 },
-  });
-
-  if (!response.ok) throw new Error('Unable to fetch posts!');
-
-  return response.json();
-}
 
 export const metadata: Metadata = {
   title: 'Blog | Next App',

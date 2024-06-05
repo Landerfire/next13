10c985df86e965406c269d19378c250186d66d76
Landerfire <alanderlex@gmail.com>
2024-06-05 07:58:09 +0500

    posts, postSearch - clientside, blog - serverside

--- components/TheBlog/index.tsx
+++ components/TheBlog/index.tsx
@@ -1,27 +1,15 @@
-'use client';
-
-import { getAllPosts } from '@/services/getPosts';
-import { useEffect, useState } from 'react';
 import { PostSearch } from '../PostSearch';
 import { Posts } from '../Posts';
 
 const TheBlog = () => {
-  const [posts, setPosts] = useState<any[]>([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    getAllPosts()
-      .then(setPosts)
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
     <>
       <h1>Blog page</h1>
-      <PostSearch onSearch={setPosts} />
-      {isLoading ? <h3>Loading...</h3> : <Posts posts={posts} />}
+      <PostSearch />
+      <Posts />
     </>
   );
 };
 
 export { TheBlog };
+
